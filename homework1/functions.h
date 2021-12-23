30ea39953cb574f34c73257036d4457dd41391c7
Starovojtov-Theormech <93205544+Starovojtov-Theormech@users.noreply.github.com>
2021-12-24 00:37:18 +0300

    Create functions.h

---
+++ homework1/functions.h
@@ -0,0 +1,15 @@
+#ifndef UNTITLED_FUNCTIONS_H
+#define UNTITLED_FUNCTIONS_H
+
+struct Vector2D {
+    double x;
+    double y;
+};
+
+double operator*(Vector2D V1, Vector2D V2);
+double cosangle(Vector2D V1, Vector2D V2);
+double distance(int a, int b, int x0, int y0);
+bool isright(int x0, int y0,int x,int y);
+
+
+#endif //UNTITLED_FUNCTIONS_H
