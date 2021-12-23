1f87c95e503f1db53e462b7615d928039ca9b775
Starovojtov-Theormech <93205544+Starovojtov-Theormech@users.noreply.github.com>
2021-12-24 00:55:50 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -8,22 +8,16 @@ using namespace std;
 
 int main() {
     string file_name = "in.txt";
-    int variant = 2;
-    //cout << "Вариант: ";
-    //cin >> variant;
 
     double rightcos, leftcos;
     rightcos = leftcos = 1;
 
-    double distancee, rightdistance, leftdistance;
-    rightdistance = leftdistance = 0;
     int x0, y0;
     int x, y, Rightmost_x, Rightmost_y, Leftmost_x, Leftmost_y;
 
     std::ifstream infile(file_name);
     infile >> x0 >> y0;
 
-    if (variant == 1) {
         while (infile >> x >> y) {
 
             double coss = (x0 * x + y0 * y) / (sqrt(x * x + y * y) * sqrt(x0 * x0 + y0 * y0));
@@ -43,25 +37,6 @@ int main() {
 
             }
         }
-    } else {
-            while (infile >> x >> y) {
-                distancee = abs(distance(x0, y0, x, y));
-                if (isright(x0, y0, x, y)) {
-                    if (distancee >= rightdistance) {
-                        Rightmost_x = x;
-                        Rightmost_y = y;
-                        rightdistance = distancee;
-                    }
-                } else {
-                    if (distancee >= leftdistance) {
-                        Leftmost_x = x;
-                        Leftmost_y = y;
-                        leftdistance = distancee;
-                    }
-                }
-            }
-        }
-
 
     cout << "Leftmost: " << Leftmost_x << " " << Leftmost_y << endl;
     cout << "Rightmost: " << Rightmost_x << " " << Rightmost_y << endl;
